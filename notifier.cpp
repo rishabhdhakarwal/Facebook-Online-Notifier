9e1f49bcd09a0e33d1a7f60254ceba0251fd9fa0
Rishabh Dhakarwal <rishabhdhakarwal@gmail.com>
2018-10-22 22:40:04 +0530

    Create notifier.cpp

---
+++ notifier.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int chutiya=1;
+int piyush = 2;
+if(piyush==chutiya)
+{
+  cout<<"No, piyush is not a chutiya"<<endl;
+}
+else
+{
+  cout<<"Definitely, piyush is 100% chutiya"<<endl;
+}
